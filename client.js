dfbb69d76685caa8d9e0fd5f1fb33960d6e2fdf6
Aidan <calopter@gmail.com>
2017-10-01 18:57:03 -0400

    try express

--- client.js
+++ client.js
@@ -2,12 +2,26 @@ const WebSocket = require('ws');
 
 const ws = new WebSocket('https://osc-relay.herokuapp.com/');
 
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
 ws.on('open', function open() {
-  ws.send('something');
+  ws.send('ws opened');
 });
 
-ws.on('message', function incoming(data) {
-  console.log(data);
+process.stdin.on('data', function(message) {
+  message = message.trim();
+  ws.send(message, console.log.bind(null, 'Sent : ', message));
 });
 
+ws.on('message', function(message) {
+  console.log('Received: ' + message);
+});
 
+ws.on('close', function(code) {
+  console.log('Disconnected: ' + code);
+});
+
+ws.on('error', function(error) {
+  console.log('Error: ' + error.code);
+});
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const WebSocket = require('ws');
+const express = require('express');
 
+const app = express.createServer();
 const port = process.env.PORT;
+
+app.get('/', function(req, res) {
+	res.send('Hello World');
+});
+
+app.listen(port);
 
 const wss = new WebSocket.Server({ port: port });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
+wss.on('connection', function (ws) {
+  ws.on('message', function (message) {
     console.log('received: %s', message);
+    ws.send('heroku heard: %s', message);
   });
 
-  ws.send('something');
+  ws.send('heroku ws got a client');
 });
