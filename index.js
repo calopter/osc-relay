f08536006760d625fe043d022396e02c7c1f6a0e
Aidan <calopter@gmail.com>
2017-10-01 19:15:22 -0400

    try express

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const WebSocket = require('ws');
 const express = require('express');
+const http = require('http');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.get('/', function(req, res) {
 	res.send('Hello World');
 });
 
-app.listen(port, function () {
-  console.log('started express')}
-);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-const wss = new WebSocket.Server({ port: port });
+server.listen(port, function () {
+  console.log('Listening on %d', server.address().port)
+});
 
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
