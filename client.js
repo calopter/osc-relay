7868732a15c525ad937b851792c7dce7e1e74e12
cynlic <montogmeryh@kenyon.edu>
2017-10-02 14:17:53 -0500

    Fixed the doubled port and disconnection issues. Users can now disconnect and reconnect at will.

--- client.js
+++ client.js
@@ -8,40 +8,40 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 // Check if user has a name
-usernameSet = process.env.OSC_NAME_SET || "false";
+usernameSet = process.env.OSC_NAME_SET || false;
 username = process.env.OSC_NAME || "Unkown";
 
 var devMode = false;
 var ws = null; //yes....*null*!!!
+var oscPort = null;
 
 // Parse command-line args. Will move into a function when necessary.
 if (process.argv[2] == "-d") {
     devMode = true;
-    var ws = new WebSocket('ws://localhost:8000');
+    oscPort = new osc.WebSocketPort({
+        url: "ws://localhost:8000"
+    });
 } else {
-    ws = new WebSocket('https://osc-relay.herokuapp.com/');
+     oscPort = new osc.WebSocketPort({
+         url: 'https://osc-relay.herokuapp.com/'
+    });
+   // ws = new WebSocket('https://osc-relay.herokuapp.com/');
 }
 
-var oscPort = new osc.WebSocketPort({
-    url: "ws://localhost:8000"
-});
-
+// Open a port for OSC communication
 oscPort.open();
 
-// Send an open notification
-ws.on('open', function open() {
-    oscPort.on("ready", function () {
-        console.log("OSC port is ready and open.");
-        oscPort.send({
-            address: "/user/" + username + "/status",
-            args: "connected"
-        });
+// Send a connection  notification
+oscPort.on("ready", function () {
+    console.log("OSC port is ready and open.");
+    oscPort.send({
+        address: "/user/" + username + "/status",
+        args: "connected"
     });
 });
 
+
 // After enter is pressed on std, send the message to the websocket.
-
-
 process.stdin.on('data', function(message) {
     message = message.trim();
     var name = "";
@@ -56,8 +56,7 @@ process.stdin.on('data', function(message) {
         args: message
     });
     }
- });
-
+});
 
 oscPort.on("osc", function (oscMsg) {
     if (oscMsg.address== "/user/" + username + "/instructions/"){
@@ -67,17 +66,3 @@ oscPort.on("osc", function (oscMsg) {
     }
 });
 
-// Display broadcasted messages in cyan
-/*
-ws.on('message', function(message) {
-    console.log('\x1b[36m%s\x1b[0m', message);
-});*/
-
-ws.on('close', function(code) {
-    console.log('Disconnected: ' + code);
-    oscPort.close();
-});
-
-ws.on('error', function(error) {
-  console.log('Error: ' + error.code);
-});
--- index.js
+++ index.js
@@ -7,15 +7,27 @@ function heartbeat() {
     this.isAlive = true;
 }
 
+
 // Listen for Web Socket connections. 
-wss.on("connection", function (socket) {
+wss.on("connection", function (socket, req) {
+
+    // Check for errors
+    socket.on("error", (err) =>  {
+        console.log("Error in socket init:  ");
+        console.log(err.stack); });
+
+    // Do a ping-pong to keep server alive
     socket.isAlive = true;
     socket.on('pong', heartbeat);
+
+    // Init socket, push it into a list, which will handle broadcasting and disconnecting.
     var socketPort = new osc.WebSocketPort({
         socket: socket
     });
     ports.push(socketPort);
-    console.log( "number of ports (doubled): " + ports.length);
+    console.log( "number of users " + ports.length);
+
+    // When a message is gotten, broadcast it to all other ports
     socketPort.on("message", function (oscMsg) {
         console.log("An OSC Message was received!", oscMsg);
 
@@ -23,8 +35,20 @@ wss.on("connection", function (socket) {
             ports[i].send(oscMsg);
         }
     });
+
+    // When a socket closes, remove it from the array so that it is not sent messages
+    socketPort.on("close", function (msg){
+        console.log("A user disconnected.");
+        for (i = 0; i < ports.length; i++){
+            if (socketPort == ports[i]) {
+                ports.splice(i, 1);
+            }
+        }
+    });
 });
 
+
+// Ping Pong Code
 const interval = setInterval(function ping() {
     wss.clients.forEach(function each(ws) {
         if (ws.isAlive === false) return ws.terminate();
